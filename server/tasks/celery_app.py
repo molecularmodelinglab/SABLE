bc7753f52991716b7beeae1d4f866dcf73353cd4
idanwekhai <kelvinidanwekhai@gmail.com>
2025-11-23 01:12:35 -0500

    ccelery app config

--- server/tasks/celery_app.py
+++ server/tasks/celery_app.py
@@ -1,7 +1,6 @@
 import os
 from celery import Celery
 
-# Get configuration from environment variables
 BROKER_URL = os.getenv("CELERY_BROKER_URL", "redis://localhost:6379/0")
 RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND", "redis://localhost:6379/0")
 
@@ -22,7 +21,6 @@ celery_app.conf.update(
     timezone="UTC",
     enable_utc=True,
     task_track_started=True,
-    # Retry connection on startup
     broker_connection_retry_on_startup=True,
 )
 
